bb98e10f7c266afd21d21727aeaed259d0949725
loner <2788892716@qq.com>
2026-01-29 00:55:23 +0800

    fix sway inputs

--- pkgs/sway-unwrapped-git/default.nix
+++ pkgs/sway-unwrapped-git/default.nix
@@ -8,10 +8,10 @@
 gitOverride {
   newInputs = with final; {
     libdrm = libdrm_git;
-    wlroots = wlroots_git;
     wayland = wayland_git;
     wayland-protocols = wayland-protocols_git;
     wayland-scanner = wayland-scanner_git;
+    wlroots_0_19 = final.wlroots_git;
   };
 
   nyxKey = "sway-unwrapped_git";
